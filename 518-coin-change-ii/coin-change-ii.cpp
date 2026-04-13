249c39100132de0e3f2b74b5152194514992f493
Aryan Gupta <aryan140505@gmail.com>
2026-04-13 19:34:04 +0530

    Time: 47 ms (23.77%) | Memory: 61.7 MB (6.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -9,9 +9,28 @@ public:
         dp[i][x]=in+ex;
         return dp[i][x];
     }
+    int solveTab(vector<int>&coins,int x){
+        int n=coins.size();
+        vector<vector<long long>> dp(n+1, vector<long long>(x + 1, 0));
+        for(int j=0;j<=n;j++){
+            dp[j][0]=1;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int k=0;k<=x;k++){
+                long long in=0;
+                if(k-coins[i]>=0)
+                    in=dp[i][k-coins[i]];
+                long long ex=dp[i+1][k];
+                // dp[i][k]=in+ex;
+                dp[i][k] = min((long long)INT_MAX, in + ex);
+            }
+        }
+        return (int)dp[0][x];
+    }
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
-        int ans=solveMem(coins,0,amount,dp);
-        return ans;
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+        // int ans=solveMem(coins,0,amount,dp);
+        // return ans;
+        return solveTab(coins,amount);
     }
 };

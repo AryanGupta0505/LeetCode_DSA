0aa19da95449764210dc14570a1fec32ffa52ca9
Aryan Gupta <aryan140505@gmail.com>
2026-04-13 19:55:36 +0530

    Time: 3 ms (26.46%) | Memory: 13.5 MB (40.67%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    int solveMem(int n,vector<int>& days,vector<int>& costs,int index,vector<int>&dp){
-        if(index>=n) return 0;
-        if(dp[index]!=-1) return dp[index];
-        int op1=costs[0]+solveMem(n,days,costs,index+1,dp);
-        int i;
-        for(i=index;i<n&&days[i]<days[index]+7;i++);
-        int op2=costs[1]+solveMem(n,days,costs,i,dp);
-        for(i=index;i<n&&days[i]<days[index]+30;i++);
-        int op3=costs[2]+solveMem(n,days,costs,i,dp);
-        dp[index]=min(op1,min(op2,op3));
-        return dp[index];
+    // int solveMem(int n,vector<int>& days,vector<int>& costs,int index,vector<int>&dp){
+    //     if(index>=n) return 0;
+    //     if(dp[index]!=-1) return dp[index];
+    //     int op1=costs[0]+solveMem(n,days,costs,index+1,dp);
+    //     int i;
+    //     for(i=index;i<n&&days[i]<days[index]+7;i++);
+    //     int op2=costs[1]+solveMem(n,days,costs,i,dp);
+    //     for(i=index;i<n&&days[i]<days[index]+30;i++);
+    //     int op3=costs[2]+solveMem(n,days,costs,i,dp);
+    //     dp[index]=min(op1,min(op2,op3));
+    //     return dp[index];
+    // }
+    int solveTab(int n,vector<int>&days,vector<int>&costs){
+        vector<int>dp(n+1,INT_MAX);
+        dp[n]=0;
+        for(int k=n-1;k>=0;k--){
+            int op1=costs[0]+dp[k+1];
+            int i;
+            for(i=k;i<n&&days[i]<days[k]+7;i++);
+            int op2=costs[1]+dp[i];
+            for(i=k;i<n&&days[i]<days[k]+30;i++);
+            int op3=costs[2]+dp[i];
+            dp[k]=min(op1,min(op2,op3));
+        }
+        return dp[0];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        vector<int>dp(n+1,-1);
-        return solveMem(n,days,costs,0,dp);
+        // vector<int>dp(n+1,-1);
+        // return solveMem(n,days,costs,0,dp);
+        return solveTab(n,days,costs);
     }
 };

d550eefc32928ca2d82f4ac3822c8c5bc4792d60
Aryan Gupta <aryan140505@gmail.com>
2026-04-19 18:33:03 +0530

    Time: 4 ms (44.16%) | Memory: 18.4 MB (64.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,46 +8,78 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Compare{
+    public:
+        bool operator()(ListNode* a,ListNode* b){
+            return a->val>b->val;
+        }
+};
 class Solution {
 public:
-    ListNode* merge(ListNode* first,ListNode* second){
-        if(first==NULL){
-            return second;
+    // ListNode* merge(ListNode* first,ListNode* second){
+    //     if(first==NULL){
+    //         return second;
+    //     }
+    //     if(second==NULL){
+    //         return first;
+    //     }
+    //     ListNode* ans=new ListNode(-1);
+    //     ListNode* temp=ans;
+    //     while(first!=NULL&&second!=NULL){
+    //         if(first->val<second->val){
+    //             temp->next=first;
+    //             temp=first;
+    //             first=first->next;
+    //         }
+    //         else{
+    //             temp->next=second;
+    //             temp=second;
+    //             second=second->next;
+    //         }
+    //     }
+    //     while(first!=NULL){
+    //         temp->next=first;
+    //         temp=first;
+    //         first=first->next;
+    //     }
+    //     while(second!=NULL){
+    //         temp->next=second;
+    //         temp=second;
+    //         second=second->next;
+    //     }
+    //     ans=ans->next;
+    //     return ans;
+    // }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // ListNode* head = NULL;
+        // for (int i = 0; i < lists.size(); i++) {
+        //     head = merge(head, lists[i]);
+        // }
+        // return head;
+        priority_queue<ListNode*,vector<ListNode*>,Compare> minHeap;
+        int k=lists.size();
+        if(k==0) return NULL;
+        for(int i=0;i<k;i++){
+            if(lists[i]!=NULL){
+                minHeap.push(lists[i]);
+            }
         }
-        if(second==NULL){
-            return first;
-        }
-        ListNode* ans=new ListNode(-1);
-        ListNode* temp=ans;
-        while(first!=NULL&&second!=NULL){
-            if(first->val<second->val){
-                temp->next=first;
-                temp=first;
-                first=first->next;
+        ListNode* head =NULL;
+        ListNode* tail=NULL;
+        while(minHeap.size()>0){
+            ListNode* top=minHeap.top();
+            minHeap.pop();
+            if(top->next!=NULL){
+                minHeap.push(top->next);
+            }
+            if(head==NULL){
+                head=top;
+                tail=top;
             }
             else{
-                temp->next=second;
-                temp=second;
-                second=second->next;
+                tail->next=top;
+                tail=top;
             }
-        }
-        while(first!=NULL){
-            temp->next=first;
-            temp=first;
-            first=first->next;
-        }
-        while(second!=NULL){
-            temp->next=second;
-            temp=second;
-            second=second->next;
-        }
-        ans=ans->next;
-        return ans;
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* head = NULL;
-        for (int i = 0; i < lists.size(); i++) {
-            head = merge(head, lists[i]);
         }
         return head;
     }

2d888d0257d4f97c12ac89b2d4fa15fa0f500637
Aryan Gupta <aryan140505@gmail.com>
2026-04-19 18:50:41 +0530

    Time: 107 ms (35.85%) | Memory: 51.2 MB (44.47%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,53 @@
+class Node{
+    public:
+        int data;
+        int row;
+        int col;
+        Node(int d,int r,int c){
+            data=d;
+            row=r;
+            col=c;
+        }
+};
+class Compare{
+    public:
+        bool operator()(Node* a,Node* b){
+            return a->data>b->data;
+        }
+};
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        int k=nums.size();
+        priority_queue<Node*,vector<Node*>,Compare> minHeap;
+        for(int i=0;i<k;i++){
+            int element=nums[i][0];
+            mini=min(mini,element);
+            maxi=max(maxi,element);
+            minHeap.push(new Node(element,i,0));
+        }
+        int start=mini,end=maxi;
+        while(minHeap.size()>0){
+            Node* temp=minHeap.top();
+            minHeap.pop();
+            mini=temp->data;
+            if(maxi-mini<end-start){
+                end=maxi;
+                start=mini;
+            }
+            if(temp->col+1<nums[temp->row].size()){
+                maxi=max(maxi,nums[temp->row][temp->col+1]);
+                minHeap.push(new Node(nums[temp->row][temp->col+1],temp->row,temp->col+1));
+            }
+            else{
+                break;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(start);
+        ans.push_back(end);
+        return ans;
+    }
+};

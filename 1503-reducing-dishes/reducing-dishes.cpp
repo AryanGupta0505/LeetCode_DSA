d21f051a266238508c9ce14d2d79ad0a49b6282b
Aryan Gupta <aryan140505@gmail.com>
2026-04-28 15:15:41 +0530

    Time: 7 ms (48.44%) | Memory: 12.4 MB (52.97%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -7,23 +7,38 @@ public:
     //     int exclude=solveMem(satisfaction,index+1,time,dp);
     //     return dp[index][time]=max(include,exclude);
     // }
-    int solveTab(vector<int>& satisfaction){
+    // int solveTab(vector<int>& satisfaction){
+    //     int n=satisfaction.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     for(int index=n-1;index>=0;index--){
+    //         for(int time=index;time>=0;time--){
+    //             int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
+    //             int exclude=dp[index+1][time];
+    //             dp[index][time]=max(include,exclude);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    int solveSpace(vector<int>& satisfaction){
         int n=satisfaction.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
         for(int index=n-1;index>=0;index--){
             for(int time=index;time>=0;time--){
-                int include=satisfaction[index]*(time+1)+dp[index+1][time+1];
-                int exclude=dp[index+1][time];
-                dp[index][time]=max(include,exclude);
+                int include=satisfaction[index]*(time+1)+next[time+1];
+                int exclude=next[time];
+                curr[time]=max(include,exclude);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
         // int n=satisfaction.size();
         // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         // return solveMem(satisfaction,0,0,dp);
-        return solveTab(satisfaction);
+        // return solveTab(satisfaction);
+        return solveSpace(satisfaction);
     }
 };

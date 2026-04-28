676cbe92c49c3e76c9e5218aca8920efc402bcb5
Aryan Gupta <aryan140505@gmail.com>
2026-04-28 15:05:47 +0530

    Time: 68 ms (57.14%) | Memory: 191.7 MB (91.18%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -1,25 +1,79 @@
 class Solution {
 public:
-    int solveMem(vector<int>& obstacles,int currlane,int currpos,vector<vector<int>>&dp){
+    // int solveMem(vector<int>& obstacles,int currlane,int currpos,vector<vector<int>>&dp){
+    //     int n=obstacles.size()-1;
+    //     if(currpos==n) return 0;
+    //     if(dp[currlane][currpos]!=-1) return dp[currlane][currpos];
+    //     if(obstacles[currpos+1]!=currlane){
+    //         return solveMem(obstacles,currlane,currpos+1,dp);
+    //     }
+    //     else{
+    //         int ans=INT_MAX;
+    //         for(int i=1;i<=3;i++){
+    //             if(currlane!=i&& obstacles[currpos]!=i){
+    //                 ans=min(ans,1+solveMem(obstacles,i,currpos,dp));
+    //             }
+    //         }
+    //         dp[currlane][currpos]=ans;
+    //     }
+    //     return dp[currlane][currpos];
+    // }
+    // int solveTab(vector<int>&obstacles){
+    //     int n=obstacles.size()-1;
+    //     vector<vector<int>>dp(4,vector<int>(obstacles.size(),1e9));
+    //     dp[0][n]=0;
+    //     dp[1][n]=0;
+    //     dp[2][n]=0;
+    //     dp[3][n]=0;
+    //     for(int currpos=n-1;currpos>=0;currpos--){
+    //         for(int currlane=1;currlane<=3;currlane++){
+    //             if(obstacles[currpos+1]!=currlane){
+    //                 dp[currlane][currpos]=dp[currlane][currpos+1];
+    //             }
+    //             else{
+    //                 int ans=1e9;
+    //                 for(int i=1;i<=3;i++){
+    //                     if(currlane!=i&&obstacles[currpos]!=i){
+    //                         ans=min(ans,1+dp[i][currpos+1]);
+    //                     }
+    //                 }
+    //                 dp[currlane][currpos]=ans;
+    //             }
+    //         }
+    //     }
+    //     return min(dp[2][0],min(1+dp[1][0],1+dp[3][0]));
+    // }
+    int solveSpace(vector<int>& obstacles){
         int n=obstacles.size()-1;
-        if(currpos==n) return 0;
-        if(dp[currlane][currpos]!=-1) return dp[currlane][currpos];
-        if(obstacles[currpos+1]!=currlane){
-            return solveMem(obstacles,currlane,currpos+1,dp);
-        }
-        else{
-            int ans=INT_MAX;
-            for(int i=1;i<=3;i++){
-                if(currlane!=i&& obstacles[currpos]!=i){
-                    ans=min(ans,1+solveMem(obstacles,i,currpos,dp));
+        vector<int> curr(4,INT_MAX);
+        vector<int> next(4,INT_MAX);
+        next[0]=0;
+        next[1]=0;
+        next[2]=0;
+        next[3]=0;
+        for(int currpos=n-1;currpos>=0;currpos--){
+            for(int currlane=1;currlane<=3;currlane++){
+                if(obstacles[currpos+1]!=currlane){
+                    curr[currlane]=next[currlane];
+                }
+                else{
+                    int ans=1e9;
+                    for(int i=1;i<=3;i++){
+                        if(currlane!=i&&obstacles[currpos]!=i){
+                            ans=min(ans,1+next[i]);
+                        }
+                    }
+                    curr[currlane]=ans;
                 }
             }
-            dp[currlane][currpos]=ans;
+            next=curr;
         }
-        return dp[currlane][currpos];
+        return min(next[2],min(1+next[3],1+next[1]));
     }
     int minSideJumps(vector<int>& obstacles) {
-        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
-        return solveMem(obstacles,2,0,dp);
+        // vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
+        // return solveMem(obstacles,2,0,dp);
+        // return solveTab(obstacles);
+        return solveSpace(obstacles);
     }
 };

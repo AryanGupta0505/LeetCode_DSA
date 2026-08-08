d4ae37ea46d2cd64b5da66b89d1c440bf531e181
Aryan Gupta <aryan140505@gmail.com>
2026-08-08 23:36:31 +0530

    Time: 2 ms (50.12%) | Memory: 9.9 MB (11.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
         unordered_map<char,int> count;
         for(int i=0;i<s.length();i++){
             count[s[i]]++;
-        }
-        for(int i=0;i<t.length();i++){
             count[t[i]]--;
         }
         for(auto i:count){

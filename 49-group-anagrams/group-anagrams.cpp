162f2ab02368c4d39b71e8fce81b85a57a91a8c0
Aryan Gupta <aryan140505@gmail.com>
2026-08-09 01:18:27 +0530

    Time: 19 ms (44.30%) | Memory: 26.2 MB (28.97%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        unordered_map<char,int> count;
+        for(int i=0;i<s.length();i++){
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for(auto i:count){
+            if(i.second!=0) return false;
+        }
+        return true;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //1st sol -tle
+        // vector<vector<string>> ans;
+        // vector<bool> used(strs.size(), false);
+        // for(int i = 0; i < strs.size(); i++) {
+        //     if(used[i])
+        //         continue;
+        //     vector<string> group;
+        //     group.push_back(strs[i]);
+        //     used[i] = true;
+        //     for(int j = i + 1; j < strs.size(); j++) {
+        //         if(!used[j] && isAnagram(strs[i], strs[j])) {
+        //             group.push_back(strs[j]);
+        //             used[j] = true;
+        //         }
+        //     }
+        //     ans.push_back(group);
+        // }
+        // return ans;
+        //2nd sol
+        unordered_map<string, vector<string>> mp;
+        for(string s:strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto i: mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
+
+    }
+};

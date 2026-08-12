0c007efa3dcb1d86213d4c25006ca1fe3d502bc5
Aryan Gupta <aryan140505@gmail.com>
2026-08-12 22:41:22 +0530

    Time: 5 ms (23.49%) | Memory: 20 MB (13.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -34,23 +34,48 @@ public:
         //     pq.pop();
         // }
         //3rd
+        // unordered_map<int, int> freq;
+        // // Count frequencies
+        // for(int num : nums) {
+        //     freq[num]++;
+        // }
+        // // Min heap: {frequency, number}
+        // priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        // for(auto &it : freq) {
+        //     pq.push({it.second, it.first});
+        //     if(pq.size() > k) {
+        //         pq.pop();
+        //     }
+        // }
+        // vector<int> ans;
+        // while(!pq.empty()) {
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return ans;
+        //4th
+        int n = nums.size();
         unordered_map<int, int> freq;
-        // Count frequencies
+        // Step 1: Count frequencies
         for(int num : nums) {
             freq[num]++;
         }
-        // Min heap: {frequency, number}
-        priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+        // Step 2: Create buckets
+        vector<vector<int>> bucket(n + 1);
+        // Step 3: Put numbers into frequency buckets
         for(auto &it : freq) {
-            pq.push({it.second, it.first});
-            if(pq.size() > k) {
-                pq.pop();
-            }
+            int num = it.first;
+            int count = it.second;
+            bucket[count].push_back(num);
         }
+        // Step 4: Traverse from highest frequency
         vector<int> ans;
-        while(!pq.empty()) {
-            ans.push_back(pq.top().second);
-            pq.pop();
+        for(int i = n; i >= 1 && ans.size() < k; i--) {
+            for(int num : bucket[i]) {
+                ans.push_back(num);
+                if(ans.size() == k)
+                    break;
+            }
         }
         return ans;
     }
